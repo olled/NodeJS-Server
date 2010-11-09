f2877ed284ac0b5ab55fef712050a9b304ce4156
olled <olle.dahlstrom@gmail.com>
2010-11-09 07:18:21 +0100

    Initial JSON and HTML support for GET

--- server.js
+++ server.js
@@ -1,19 +1,42 @@
 var createServer = require("http").createServer;
-var readFile = require("fs").readFile;
-var sys = require("sys");
-var url = require("url");
+var url = require('url');
+var parse = require('url').parse
+var serverUtils = require("utils");
+var queryString = require("querystring");
 
-var server = createServer(function (req, res) {
-  if (req.method === "GET" || req.method === "HEAD") {
-    
-    res.simpleJSON = function (code, obj) {
-      var body = JSON.stringify(obj);
-      res.writeHead(code, { "Content-Type": "text/json"
-                          , "Content-Length": body.length
-                          });
-      res.end(body);
-    };
 
-    handler(req, res);
-  }
-});
+createServer(function (request, response) {
+	if (request.method == 'GET') {
+		
+		var contentType = 'text/html';
+		var responseText = 'Hello World\n';
+		
+		if (url.parse(request.url).pathname.lastIndexOf('/json') > -1)
+		{
+			contentType = 'text/json';
+			responseText = JSON.stringify({id:1, name:'Olle'});
+		}
+		else if (url.parse(request.url).pathname.lastIndexOf('/html') > -1)
+		{
+			test = ''
+		}
+		
+		response.writeHead(200, {
+			'Content-Type': contentType,
+			"Content-Length": responseText.length
+		});
+		response.end(responseText);
+	}
+	else if (request.method == 'POST')
+	{
+		request.body = '';
+	    request.addListener('data', function (chunk) {
+	       
+	      })
+	      .addListener('end', function () {
+				
+	      });
+	}
+}).listen(8080);
+
+console.log('Running at http://127.0.0.1:8080/');
