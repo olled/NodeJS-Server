2554ef61f1b14936e7d3a2a3196033b9b0d3afe0
olled <olle.dahlstrom@gmail.com>
2010-12-02 16:34:51 +0100

    testing out nodeunit

--- test/test-test.js
+++ test/test-test.js
@@ -1,10 +1,19 @@
-exports["test some stuff"] = function(test){
-    test.expect(1);
-    test.ok(true, "this assertion should pass");
-    test.done();
-};
-
-exports["test some other stuff"] = function(test){
-    test.ok(false, "this assertion should fail");
-    test.done();
-};
+var testCase = require('nodeunit').testCase;
+module.exports = testCase({
+    setUp: function (callback) {
+        this.foo = 'bar';
+        callback();
+    },
+    tearDown: function (callback) {
+        // clean up
+        callback();
+    },
+    "testing for the thing": function (test) {
+        test.equals(this.foo, 'bar');
+        test.done();
+    },
+	different_type_of_name_of_test_case: function (test) {
+        test.equals(this.foo, 'bar');
+        test.done();
+    }
+});
